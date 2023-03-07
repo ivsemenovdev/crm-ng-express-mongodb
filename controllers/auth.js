fa0fe71ec8372c5e2a8de2b9ad7a53df15f81a98
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-07 23:35:41 +0300

    feat: add auth route to app

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
 module.exports.login = function (req, res) {
     res.status(200).json({
-        login: 'from controller'
-    })
+        login: 'Login from controller'
+    });
+}
+
+module.exports.register = function (req, res) {
+    res.status(200).json({
+        register: 'Register from controller'
+    });
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const controller = require("../controllers/auth");
 const router = express.Router();
 
 // localhost:3000/api/auth/login
-router.get('/login', controller.login)
+router.get('/login', controller.login);
+// localhost:3000/api/auth/register
+router.get('/register', controller.register);
 
 module.exports = router
