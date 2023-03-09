b6603cdfec4238ce12b88a15e12260d9b21f7c3d
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-09 14:52:53 +0300

    feat: add registration - creating a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,51 @@
-module.exports.login = function (req, res) {
+const User = require('../models/User')
 
+module.exports.login = function (req, res) {
     res.status(200).json({
         login: {
             email: req.body.email,
             password: req.body.password
         }
     });
-
 }
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        register: 'Register from controller'
-    });
+module.exports.register = async function (req, res) {
+
+    // email password
+    // Проверка - создания пользователя
+    // const user = new User({
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    //
+    // user.save().then( () => {
+    //     console.log('User created');
+    // });
+
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        //Пользователь существует, отправляем ошибку
+        res.status(409).json({
+            message: 'Пользователь с таким email уже зарегистрирован'
+        });
+    } else {
+        //Создаём пользователя
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        //Записываем пользователя в БД
+        try {
+            await user.save();
+            //отправляем ответ в случае успеха
+            res.status(201).json(user);
+        } catch (e) {
+            //обработка ошибки
+        }
+
+
+    }
+
 }
