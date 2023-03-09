0186c7480f9f80ef1a227fb96e33870257f085ca
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-09 22:48:39 +0300

    feat: add Api - position - getByCategoryId

--- controllers/position.js
+++ controllers/position.js
@@ -1,23 +1,38 @@
-module.exports.getByCategoryId = function (req, res) {
-    res.status(200).json({
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
-    });
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(positions);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.create = function (req, res) {
-    res.status(200).json({
+module.exports.create = async function (req, res) {
+    try {
 
-    });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.status(200).json({
+module.exports.remove = async function (req, res) {
+    try {
 
-    });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.update = function (req, res) {
-    res.status(200).json({
+module.exports.update = async function (req, res) {
+    try {
 
-    });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
