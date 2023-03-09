e82da151258b7464c3d0bddb49abab12c06477cb
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-09 22:57:48 +0300

    feat: add Api - position - delete

--- controllers/position.js
+++ controllers/position.js
@@ -30,7 +30,10 @@ module.exports.create = async function (req, res) {
 
 module.exports.remove = async function (req, res) {
     try {
-
+        await Position.remove({_id: req.params.id});
+        res.status(200).json({
+            message: 'Позиция была удалена'
+        })
     } catch (e) {
         errorHandler(res, e);
     }
