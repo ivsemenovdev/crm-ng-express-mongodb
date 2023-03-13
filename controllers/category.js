f6691fb38f4995dc46f51bb6ef8209972cda158c
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-13 09:26:18 +0300

    feat: add API - category - getAll, getById, remove

--- controllers/category.js
+++ controllers/category.js
@@ -1,31 +1,51 @@
-module.exports.getAll = function (req, res) {
-    res.json({
-        message: 'categories'
-    });
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.getById = function (req, res) {
-    res.status(200).json({
-
-    });
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.status(200).json({
-
-    });
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({
+            message: 'Категория удалена'
+        });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.create = function (req, res) {
-    res.status(200).json({
+    try {
 
-    });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 module.exports.update = function (req, res) {
-    res.status(200).json({
+    try {
 
-    });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
 
