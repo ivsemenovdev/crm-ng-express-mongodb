49ae17e165529f4c0ce508027ce46c0984e90f4e
Ivan Semenov <ivsemenovdev@gmail.com>
2023-03-13 19:18:09 +0300

    feat: add two layouts - auth, site to app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthLayoutComponent} from "./layouts/auth-layout/auth-layout.component";
+import {SiteLayoutComponent} from "./layouts/site-layout/site-layout.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: 'login', component: LoginPageComponent}
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, children: [
+
+    ]
+  }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './layouts/site-layout/site-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent
   ],
   imports: [
     BrowserModule,
